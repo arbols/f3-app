6569b821fffbf163d06913e98004bb5a50aeabc0
Arturo Bolivar <arturobol@gmail.com>
2025-02-11 19:52:09 -0500

    actualizacion

--- src/components/About.js
+++ src/components/About.js
@@ -5,22 +5,22 @@ import { Link } from 'react-router-dom';
 function About() {
   return (
     <Container>
-      <Card className="text-center mt-5">
-        <Card.Header>Acerca de</Card.Header>
-        <Card.Body>
-          <Card.Title>Información sobre nosotros.</Card.Title>
-          <Card.Text>{}
-                Arturo Jose Bolivar Sanchez<br />
-                Actividad Extraordinaria<br />
-                Master En Ingenieria del Software y Sistemas Informaticos<br />
-                Desarrollo Web Full Stack<br />
-                MISSI - PER 9208 - Febrero 2024<br />
+      <Card className="about mt-5 text-center"> 
+        <Card.Header className="about__header">Acerca de</Card.Header> 
+        <Card.Body className="about__body"> 
+          <Card.Title className="about__title">Información sobre nosotros.</Card.Title> 
+          <Card.Text className="about__text"> {}
+            Arturo Jose Bolivar Sanchez<br />
+            Actividad Extraordinaria<br />
+            Master En Ingenieria del Software y Sistemas Informaticos<br />
+            Desarrollo Web Full Stack<br />
+            MISSI - PER 9208 - Febrero 2024<br />
           </Card.Text>
-          <Link to="/peliculas"> {}
+          <Link to="/peliculas" className="about__link"> 
             <Button variant="primary">Ir a Coleccion de Peliculas</Button>
           </Link>
         </Card.Body>
-        <Card.Footer className="text-muted">© 2025 ArbolS Tech</Card.Footer>
+        <Card.Footer className="about__footer text-muted">© 2025 ArbolS Tech</Card.Footer> 
       </Card>
     </Container>
   );
